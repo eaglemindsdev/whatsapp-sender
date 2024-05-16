2d6f20683b618d84c2b255ff3fe32564bf017ac1
VadivelanTEST <velanms1993@gmail.com>
2024-05-16 19:53:38 +0530

    data update today 1

--- src/WhatsAppServiceProvider.php
+++ src/WhatsAppServiceProvider.php
@@ -10,23 +10,14 @@ class WhatsAppServiceProvider extends ServiceProvider
 {
     public function register()
     {
-                $this->app->bind(WhatsAppSender::class, function ($app) {
-            // Retrieve configuration values from your Laravel configuration
-            $apiUrl = config('whatsapp.api_url');
-            $apiKey = config('whatsapp.api_key');
-            $authKey = config('whatsapp.auth_key');
-            $templateId = config('whatsapp.template_id');
-            $countryCode = config('whatsapp.country_code');
-
-            return new WhatsAppSender($apiUrl, $apiKey, $authKey, $templateId, $countryCode);
-        });
+        $this->mergeConfigFrom(__DIR__ . '/../whatsapp-config.php', 'whatsapp');
     }
 
     public function boot()
     {
         // Publish configuration file during installation
         $this->publishes([
-            __DIR__ . '/../whatsapp-config.php' => config_path('whatsapp-config.php'),
+            __DIR__ . '/../whatsapp-config.php' => config_path('whatsapp.php'),
         ], 'whatsapp-config');
     }
 }
